b8054c867787d58ed313ecaa6366ea9d4c3ddefc
NikithaKoppu <35382140+NikithaKoppu@users.noreply.github.com>
2018-05-20 16:42:18 -0400

    renamed project and added create course and delete course partially works

---
+++ src/components/Message.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+    constructor(props) { super(props); }
+    render() {
+        return (<h1>{this.props.message}</h1>
+        )}}
+export default Message;
--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -3,12 +3,17 @@ import CourseCard from '../components/CourseCard';
 import ModuleList from './ModuleList'
 import LessonTabs from './LessonTabs'
 import CourseEditor from './CourseEditor'
+import CourseList from "./CourseList";
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
 
 export default class CourseManager extends React.Component {
     render() {
         return (
             <div className="container-fluid">
                 <h1>Course Manager</h1>
+                <CourseList/>
+
                 <CourseEditor/>
                 <LessonTabs/>
                 <ModuleList/>
